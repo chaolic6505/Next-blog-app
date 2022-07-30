b3c25eda7811c95cd76088a85ac0406f298fe5f7
Steven <chaolic6505@gmail.com>
2022-07-29 23:12:27 -0700

    remove unused

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,7 +22,6 @@ export default function Home({ posts }: Props) {
 
       <Header />
       <Banner />
-      {/* <Posts posts={posts}/> */}
 
       <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6">
         {posts.map((post) => {
