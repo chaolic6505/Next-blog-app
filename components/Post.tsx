39f8726df0512babad0cbe25b89ffecc81ae615a
Steven <chaolic6505@gmail.com>
2022-07-30 12:12:55 -0700

    update image null check

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,36 +1,57 @@
-import React from 'react'
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
 
-import { Post as _Post } from '../typing';
-import { urlFor } from '../sanity';
+import { Post as _Post } from "../typing";
+import { urlFor } from "../sanity";
 
 interface Props {
     post: _Post;
 }
 
-export default function Post({ post } : Props) {
+export default function Post({ post }: Props) {
+    console.log(post, "post");
     return (
-        <Link key={post._id} href={`/post/${post.slug.current}`}>
-            <div className="group cursor-pointer overflow-hidden rounded-lg border">
-                <img
-                    alt="post-image"
-                    src={urlFor(post.mainImage).url()!}
-                    className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
-                />
-                <div className="flex justify-between bg-white p-5">
-                    <div>
-                        <p className="text-lg font-bold">{post.title}</p>
-                        <p>
-                            {post.description} by {post.author.name}
-                        </p>
+        <div className="bg-slate-50">
+            <Link key={post._id} href={`/post/${post.slug.current}`} className="bg-slate-50">
+                <div className="group cursor-pointer overflow-hidden rounded-lg">
+                    {post.mainImage ? (
+                        <img
+                            alt="post"
+                            src={urlFor(post.mainImage).url()}
+                            className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+                        />
+                    ) : (
+                        <img
+                            alt="post"
+                            src="/images/logo/medium-logo-black-rgb.svg"
+                            className="h-60 w-1/2object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+                        />
+                    )}
+
+                    <div className="flex justify-between bg-slate-50 p-5">
+                        <div>
+                            <p className="text-lg font-bold">{post.title}</p>
+                            <p>
+                                {post.description} by {post.author.name}
+                            </p>
+                        </div>
+                        {post.author.image ? (
+                            <img
+                                alt="author"
+                                className="h-12 w-12 rounded-full"
+                                src={urlFor(post.author.image).url()!}
+                            />
+                        ) : (
+                            <img
+                                alt="post"
+                                src="/images/defaults/robot.jpeg"
+                                className="h-12 w-12 rounded-full"
+                            />
+                        )}
                     </div>
-                    <img
-                        alt="author"
-                        className="h-12 w-12 rounded-full"
-                        src={urlFor(post.author.image).url()!}
-                    />
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
     );
 }
