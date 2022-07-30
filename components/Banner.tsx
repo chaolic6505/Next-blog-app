9daf67e2461df9e4c76349b201168a9984cceae2
Steven <chaolic6505@gmail.com>
2022-07-29 21:54:17 -0700

    escape  " ' "

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -3,21 +3,21 @@ import React from "react";
 export default function Banner() {
     return (
         <div className="flex items-center justify-between border-y border-black bg-yellow-400 py-10 lg:py-5">
-        <div className="space-y-5 px-10">
-            <h1 className="max-w-xl font-serif text-8xl">
-                Stay curious.
-            </h1>
-            <h2 className="text-2xl">
-                Discover stories, thinking, and expertise from writers on any topic.
-            </h2>
-            <h3 className="font-serif text-xl">
-                <span className="underline decoration-black decoration-1">
-                    Medium
-                </span>{' '}
-                is a place to write, read and connect
-            </h3>
-            <h4>
-                It's easy and free to post your thinking on any topic and connect
+            <div className="space-y-5 px-10">
+                <h1 className="max-w-xl font-serif text-8xl">
+                    Stay curious.
+                </h1>
+                <h2 className="text-2xl">
+                    Discover stories, thinking, and expertise from writers on any topic.
+                </h2>
+                <h3 className="font-serif text-xl">
+                    <span className="underline decoration-black decoration-1">
+                        Medium
+                    </span>{' '}
+                    is a place to write, read and connect
+                </h3>
+                <h4>
+                    It&rsquo;s easy and free to post your thinking on any topic and connect
                 with millions of readers.
             </h4>
         </div>
