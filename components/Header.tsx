151d917bf86ccff7ec635407702f81377092a71d
Steven <chaolic6505@gmail.com>
2022-07-29 21:29:16 -0700

    formatting

--- components/Header.tsx
+++ components/Header.tsx
@@ -25,21 +25,19 @@ const leftSide = () => {
 const rightSide = () => {
   return (
     <div className="flex items-center space-x-5 text-green-600">
-        <h3>Sign In</h3>
-        <h3 className="rounded-full border border-green-600 px-4 py-1">
-          Get Started
-        </h3>
-      </div>
+      <h3>Sign In</h3>
+      <h3 className="rounded-full border border-green-600 px-4 py-1">
+        Get Started
+      </h3>
+    </div>
   );
 };
-
 
 export default function Header() {
   return (
     <header className="mx-auto flex max-w-7xl justify-between p-5">
-      { leftSide() }
-      { rightSide() }
-
+      {leftSide()}
+      {rightSide()}
     </header>
   );
 }
