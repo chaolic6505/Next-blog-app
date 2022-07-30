560bd392cba641db161ec9e1976e2badb83cd51b
Steven <chaolic6505@gmail.com>
2022-07-29 21:08:56 -0700

    Add header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,17 @@
-import type { NextPage } from 'next'
-import Header from '../components/Header';
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Header from "../components/Header";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Header />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
