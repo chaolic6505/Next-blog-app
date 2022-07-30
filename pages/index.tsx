43459be8eed05ac98bbdbb56cb11073d60fd0d31
Steven <chaolic6505@gmail.com>
2022-07-29 23:05:52 -0700

    Update package

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from "next";
-import { sanityClient } from '../sanity';
+import Link from 'next/link';
+import { sanityClient, urlFor } from '../sanity';
 
 import Header from "../components/Header";
 import Banner from "../components/Banner";
@@ -12,6 +12,7 @@ interface Props {
 }
 
 export default function Home({ posts }: Props) {
+  console.log(posts)
   return (
     <div className="mx-auto max-w-7xl">
       <Head>
@@ -22,6 +23,37 @@ export default function Home({ posts }: Props) {
       <Header />
       <Banner />
       {/* <Posts posts={posts}/> */}
+
+      <div className="grid grid-cols-1 gap-3 p-2 sm:grid-cols-2 md:gap-6 md:p-6">
+        {posts.map((post) => {
+          return (
+            <Link key={post._id} href={`/post/${post.slug.current}`}>
+              <div className="group cursor-pointer overflow-hidden rounded-lg border">
+                <img
+                  className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+                  src={urlFor(post.mainImage).url()!}
+                  alt=""
+                />
+                <div className="flex justify-between bg-white p-5">
+                  <div>
+                    <p className="text-lg font-bold">{post.title}</p>
+                    <p>
+                      {post.description} by {post.author.name}
+                    </p>
+                  </div>
+
+                  <img
+                    className="h-12 w-12 rounded-full"
+                    src={urlFor(post.author.image).url()!}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+
     </div>
   );
 };
