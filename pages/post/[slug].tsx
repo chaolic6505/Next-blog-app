00d0bd63a98ef1e6dd931197832da7a086de0fc6
Steven <chaolic6505@gmail.com>
2022-07-31 22:00:22 -0700

    add comment form and foramtting

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -94,6 +94,38 @@ function Post({ post }: PostProps) {
                     />
                 </div>
             </article>
+            <hr className="max-w-lg my-5 mx-auto border border-slate-500" />
+            <form className="flex flex-col p-5 max-w-2xl mx-auto mb-10">
+                <h3 className="text-sm text-slate-600">
+                    Enjoyed this article?
+                </h3>
+                <h4 className="text-3xl font-bold">Leave a comment below!</h4>
+                <hr className="py-3 mt-2" />
+
+                <label className="block mb-5">
+                    <span className="text-gray-700">Name</span>
+                    <input
+                        className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-cyan-500 outline-none focus:ring"
+                        type="text"
+                    />
+                </label>
+
+                <label className="block mb-5">
+                    <span className="text-gray-700">Email</span>
+                    <input
+                        className="shadow border rounded py-2 px-3 form-input mt-1 block w-full ring-cyan-500 outline-none focus:ring "
+                        type="email"
+                    />
+                </label>
+
+                <label className="block mb-5">
+                    <span className="text-gray-700">Comment</span>
+                    <textarea
+                        className="shadow border rounded py-3 px-3 form-textarea mt-1 block w-full ring-cyan-500 outline-none focus:ring"
+                        rows={5}
+                    />
+                </label>
+            </form>
         </main>
     );
 }
