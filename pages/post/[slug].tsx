a4eac1893db6be7ea510595a9135b815eef7ebf5
Steven <chaolic6505@gmail.com>
2022-07-31 19:40:13 -0700

    Update slug.tsx

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { GetStaticProps } from "next";
+import { urlFor } from "../../sanity";
+
+import Header from "../../components/Header";
+import { Post as typePost } from "../../typing";
+
+import PostsServices from "../../lib/http-services/post-http-service";
+
+interface PostProps {
+    post: typePost;
+}
+
+export const Post = ({ post }: PostProps) => {
+    console.log(post, "post");
+    return (
+        <main>
+            <Header />
+        </main>
+    );
+};
+
+export const getStaticPaths = () => {
+    return PostsServices.fetchPostPath();
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const post = await PostsServices.fetchPostProps({
+        slug: params?.slug,
+    });
+
+    if (!post) {
+        return {
+            notFound: true,
+        };
+    }
+    return {
+        props: {
+            post,
+        },
+    };
+};
+
+export default Post;
