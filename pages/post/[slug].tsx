e06fad9704b2527bd3f95176cce0ad472477d416
Steven <chaolic6505@gmail.com>
2022-07-31 21:04:07 -0700

    show title, description, author avatar works

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -11,14 +11,49 @@ interface PostProps {
     post: Post;
 }
 
-function Post ({ post }: PostProps)  {
+function Post({ post }: PostProps) {
     console.log(post, "post");
     return (
         <main>
             <Header />
+            <img
+                alt="post-image"
+                src={urlFor(post.mainImage).url()!}
+                className="w-full h-40 object-cover"
+            />
+            <article className="max-w-3xl mx-auto p-5">
+                <h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+                <h2 className="text-xl font-light text-gray-500 mb-2">
+                    {post.description}
+                </h2>
+
+                <div className="flex items-center space-x-2">
+                    {post.author.image ? (
+                        <img
+                            alt="author"
+                            className="h-12 w-12 rounded-full"
+                            src={urlFor(post.author.image).url()!}
+                        />
+                    ) : (
+                        <img
+                            alt="post"
+                            src="/images/defaults/robot.jpeg"
+                            className="h-12 w-12 rounded-full"
+                        />
+                    )}
+
+                    <p className="font-extralight text-sm">
+                        <span className="text-green-600">
+                            Article post by {post.author.name}{" "}
+                        </span>{" "}
+                        - Published at{" "}
+                        {new Date(post._createdAt).toLocaleString()}
+                    </p>
+                </div>
+            </article>
         </main>
     );
-};
+}
 
 export const getStaticPaths = () => {
     return PostsServices.fetchPostPath();
