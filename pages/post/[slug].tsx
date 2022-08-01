472f20db02476c75727056a1d78628c1656ff542
Steven <chaolic6505@gmail.com>
2022-07-31 21:09:31 -0700

    add react-portable-text package to support Sanity post.body

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { GetStaticProps } from "next";
 import { urlFor } from "../../sanity";
+import PortableText from "react-portable-text";
 
 import Header from "../../components/Header";
 import { Post } from "../../typing";
@@ -49,6 +50,39 @@ function Post({ post }: PostProps) {
                         - Published at{" "}
                         {new Date(post._createdAt).toLocaleString()}
                     </p>
+                </div>
+
+                <div className="mt-10">
+                    <PortableText
+                        dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+                        projectId={process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!}
+                        content={post.body}
+                        serializers={{
+                            h1: (props: any) => (
+                                <h1
+                                    className="text-2xl font-bold my-5"
+                                    {...props}
+                                />
+                            ),
+                            h2: (props: any) => (
+                                <h2
+                                    className="text-xl font-bold my-5"
+                                    {...props}
+                                />
+                            ),
+                            li: ({ children }: any) => (
+                                <li className="ml-4 list-disc"> {children}</li>
+                            ),
+                            link: ({ href, children }: any) => (
+                                <a
+                                    href={href}
+                                    className="text-blue-500 hover:underline"
+                                >
+                                    {children}
+                                </a>
+                            ),
+                        }}
+                    />
                 </div>
             </article>
         </main>
